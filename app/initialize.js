b4c8074d0d9ad5e1b79182e4bf2e564979cb0ebb
Kevin Garcia <kevin@kgcreative.com>
2017-01-08 22:31:53 -0800

    1st pass at jquery init. WIP.

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-// do your setup here
-	var $ = require('jquery');
-	console.log('jQuery Loaded', $('body'));
+  // window.$ = $;
+
+  $('#skiplinks > a').click(function(e){
+    var href = $(this).attr('href').substr(1); // remove the #
+    var target = $('a[name="' + href + '"]');
+    $(this).blur();
+    target.focus();
+    console.log('hello skiplinks');
+  });
+
+  // Load fastclick.js for better mobile performance. See https://github.com/ftlabs/attachFastClick
+  var attachFastClick = require('fastclick');
+  attachFastClick(document.body);
+  console.log('hello fastclick');
+
+  // initialize the megamenu
+  // $('nav#primary-nav').accessibleMegaMenu();
+  
+  // hack so that the megamenu doesn't show flash of css animation after the page loads.
+  setTimeout(function () {
+      $('body').removeClass('init');
+      console.log('body initialized')
+  }, 500);
+
 });
